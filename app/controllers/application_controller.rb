1b4b7c8282034661c4f7740aebca4d85b8c50dc8
Pedro Cunha <pedromplcunha@gmail.com>
2025-12-02 11:01:57 +0000

    replaced the user for member authorizatioon in the pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_member!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit::Authorization
 
   # Pundit: allow-list approach
-  after_action :verify_authorized, except: :index, unless: :skip_pundit?
-  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+  after_action :verify_authorized, unless: :skip_authorization?
+  after_action :verify_policy_scoped, unless: :skip_pundit_scope?
 
   # Uncomment when you *really understand* Pundit!
   # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
@@ -15,7 +16,20 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
+
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
+
+  def skip_authorization?
+    skip_pundit? || action_name == "index"
+  end
+
+  def skip_pundit_scope?
+    skip_pundit? || action_name != "index"
   end
 end
