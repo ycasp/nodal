37a7499ff2f361e2d2bb53615582ff099254ab00
Pedro Cunha <pedromplcunha@gmail.com>
2025-12-02 12:23:07 +0000

    Addresses polimorphic tables created and tested -- 1 organization has 1 billing address, customer have 1 billing address and multiple shipping addresses

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,9 +6,11 @@ class Customer < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   belongs_to :organisation
+  has_many :addresses, as: :addressable, dependent: :destroy
+  has_one :billing_address, -> { billing }, class_name: "Address", as: :addressable, dependent: :destroy
+  has_many :shipping_addresses, -> { shipping }, class_name: "Address", as: :addressable, dependent: :destroy
 
   validates :company_name, presence: true
   validates :contact_name, presence: true
   validates :active, inclusion: { in: [true, false] }
-
 end
--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -2,6 +2,8 @@ class Organisation < ApplicationRecord
   has_many :org_members, dependent: :destroy
   has_many :members, through: :org_members
   has_many :customers, dependent: :destroy
+  has_many :addresses, as: :addressable, dependent: :destroy
+  has_one :billing_address, -> { billing }, class_name: "Address", as: :addressable, dependent: :destroy
 
   validates :name, presence: true
   validates :slug, presence: true, uniqueness: true, format: { with: /\A[a-z0-9-]+\z/, message: "only lowercase letters, numbers, and hyphens" }
